bf047b5011a495e173c2a42ef81c4569ab874502
Allan Lei <allanlei@helveticode.com>
2012-06-04 16:59:54 +0800

    Add files

---
+++ js/libs/jquery.parallax.js
@@ -0,0 +1,69 @@
+/**
+	Plugin: jQuery Parallax
+	Author: Allan Lei
+	Forked from https://github.com/IanLunn/jQuery-Parallax
+	
+	Licensed under the MIT:
+	http://www.opensource.org/licenses/mit-license.php
+**/
+
+(function($){
+	$.fn.parallax = function(layers, options){
+		return this.each(function(){
+			var settings = $.extend({}, {
+				parallaxClass: null,
+				layerClass: null,
+				offset: true,
+				animation: {
+					duration: 50, 
+					easing: "linear",
+					queue: false
+				}
+			}, options);
+			
+			var $parallax = $(this).addClass(settings.parallaxClass).data({
+				"layers": []
+			});
+			
+			$.each(layers, function(selector, layerOptions){
+				var layerSettings = $.extend({}, {
+					xFn: null,
+					yFn: null,
+				}, layerOptions);
+				var $layer = $parallax.find(selector).addClass(settings.layerClass);
+				$parallax.data("layers").push($layer);
+				
+				$parallax.on("update.parallax.layer", selector, function(){
+					var $layer = $(this);
+					var css = {};
+					var viewport = {
+						scroll: {
+							left: $(window).scrollLeft(),
+							top: $(window).scrollTop()
+						},
+						dimension: {
+							width: $(window).width(),
+							height: $(window).height()
+						}
+					};
+				
+					if(layerSettings.xFn){
+						css["backgroundPositionX"] = $.proxy(layerSettings.xFn, $layer)(viewport);
+					}
+					
+					if(layerSettings.yFn){
+						css["backgroundPositionY"] = $.proxy(layerSettings.yFn, $layer)(viewport);
+					}
+					
+					$layer.stop(true, false).animate(css, settings.animation);
+				});
+			});
+			
+			$parallax.on("update.parallax", function(){
+				$.each($(this).data("layers"), function(index, $layer){
+					$layer.trigger("update.parallax.layer");
+				});
+			});
+		});
+    };
+})(jQuery);
---
+++ js/script.js
@@ -0,0 +1,7 @@
+function RepositionNav(){
+	var windowHeight = $(window).height(); //get the height of the window
+	var navHeight = $('#nav').height() / 2;
+	var windowCenter = (windowHeight / 2); 
+	var newtop = windowCenter - navHeight;
+	$('#nav').css({"top": newtop}); //set the new top position of the navigation list
+}
